39fc77c1452d9c3b1e62f1c80c895d53bed519a9
rashtraIQ <rashtra.innovquant@gmail.com>
2025-09-16 12:26:50 +0530

    Initial Commit

---
+++ Task1/Task1App.swift
@@ -0,0 +1,17 @@
+//
+//  Task1App.swift
+//  Task1
+//
+//  Created by Rashtra Humane on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Task1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
