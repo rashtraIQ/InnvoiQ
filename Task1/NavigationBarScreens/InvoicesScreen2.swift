c962e146f5b98fed1c2fbcd6715f423834629dd1
rashtraIQ <rashtra.innovquant@gmail.com>
2025-11-10 17:57:01 +0530

    code updated

---
+++ Task1/NavigationBarScreens/InvoicesScreen2.swift
@@ -0,0 +1,18 @@
+//
+//  InvoicesScreen2.swift
+//  Task1
+//
+//  Created by Rashtra Humane on 10/11/25.
+//
+
+import SwiftUI
+
+struct InvoicesScreen2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InvoicesScreen2()
+}
---
+++ Task1/NavigationBarScreens/addInvoiceStructure.swift
@@ -0,0 +1,8 @@
+//
+//  addInvoiceStructure.swift
+//  Task1
+//
+//  Created by Rashtra Humane on 10/11/25.
+//
+
+import Foundation
